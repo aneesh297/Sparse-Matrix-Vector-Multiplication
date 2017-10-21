56af20db7e70cffe32389320fe8979a8965d8f6d
Aneesh <aneesh297@gmail.com>
2017-10-21 22:59:10 +0530

    cuda memory allocation and freeing done

--- proj.cu
+++ proj.cu
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -6,10 +6,11 @@
 using namespace std;
 
 // Generates a sparse matrix while keeping count of non zero elements
- float * sparse_gen(int n, int &nnz)
+ float * sparse_gen(int n, int &nnz, int &nnz_row, int &nnz_max)
 {
 	float *arr = new float[n];
 	
+	nnz_row = 0;
 
 	for (int j = 0; j < n; j++)
 	{
@@ -19,13 +20,20 @@ using namespace std;
 		if(prob>=0.7)
 		{
 			arr[j] = rand()%100 + 1;
-			nnz++;
+			nnz_row++;
 		}
 		else
 		{
 			arr[j] = 0;
 		}
 	}
+
+	if(nnz_row > nnz_max)
+	{
+		nnz_max = nnz_row;
+	}
+
+	nnz += nnz_row;
 
 	return arr;
 }
@@ -143,18 +151,34 @@ void simple_spmv(float *res, float *vect, float * values, int *col_idx, int *row
 	}
 }
 
+void calculate_bin_size(vector <vector<int> > &bins, int *nnz_row, int m)
+{
+	for(int i = 0; i < m; i++)
+	{
+		bins[nnz_row[i]].push_back(i);
+		cout<<nnz_row[i]<<" "<<i<<endl;
+	}
+}
+
+__global__ void spmv(float *values, int *col_idx, int *row_off,float * vect, float * res, int m, int n, int *bin, int bin_size)
+{
+
+}
+
 int main()
 {
 	srand (time(NULL));
 	int m = 5, n = 5;
-	int nnz = 0; 
+	int nnz = 0, nnz_row[m], nnz_max = 0; 
 	float *mat[m], *vect, *res;
 	float *values;
 	int *col_idx, *row_off;
 
+
+
 	for(int i = 0; i < m; i++)
 	{
-		mat[i] = sparse_gen(n, nnz);
+		mat[i] = sparse_gen(n, nnz, nnz_row[i], nnz_max);
 	}
 
 	cout<<"\nMatrix generated: \n";
@@ -170,6 +194,7 @@ int main()
 	col_idx = new int[nnz];
 	row_off = new int[m];
 
+	vector <vector<int> > bins(nnz_max+1);
 
 
 	to_csr(mat, values, col_idx, row_off, m, n);
@@ -183,6 +208,58 @@ int main()
 	cout<<"Result vector: \n";
 	display_vector(res, n);
 
+	calculate_bin_size(bins, nnz_row, m);
 
+	for (int i = 0; i < nnz_max+1; ++i)
+	{
+
+		cout<<"Bin Size: "<<i<<endl;
+		for(int j = 0; j < bins[i].size(); j++)
+		{
+			cout<<bins[i][j]<<" ";
+		}
+
+		cout<<endl;
+	}
+
+
+
+	// CUDA stuff
+
+	int *dcol_idx, *drow_off, *dbin;
+	float *dvect, *dres, *dvalues;
+	float milliseconds = 0;
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
+
+
+	cout<<"Allocating memory\n";
+	cudaEventRecord(start);
+	cudaMalloc((void**)&dcol_idx, (nnz)*sizeof(int));
+	cudaMalloc((void**)&drow_off, (m)*sizeof(int));
+	cudaMalloc((void**)&dvect, (n)*sizeof(int));
+	cudaMalloc((void**)&dres, (n)*sizeof(int));
+	cudaMalloc((void**)&dvalues, (nnz)*sizeof(int));
+	cudaEventRecord(stop);
+
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&milliseconds, start, stop);
+	cout<<"Memory Allocation successful: "<<milliseconds<<"ms\n";
+
+
+	cout<<"Freeing memory\n";
+	cudaEventRecord(start);
+	cudaFree(dcol_idx);
+	cudaFree(drow_off);
+	cudaFree(dvect);
+	cudaFree(dres);
+	cudaFree(dvalues);
+	cudaEventRecord(stop);
+
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&milliseconds, start, stop);
+	cout<<"Memory Freed: "<<milliseconds<<"ms\n";
 
 }
